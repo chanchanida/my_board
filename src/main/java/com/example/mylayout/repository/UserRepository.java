7f3e8318df0b80555079f8a9962c7ef6d524c27f
noahKim <noah.kim@osckorea.com>
2021-06-18 17:44:50 +0900

    Revert "개시판 만들기 - 회원가입 까지만. 로그인처리는 나중에"
    This reverts commit 3ee4f15d6371add5f1ca9e42bd8708f644f7ca1c.

--- src/main/java/com/example/mylayout/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mylayout.repository;
-
-
-import com.example.mylayout.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository  extends JpaRepository<User, Long> {
-
-}
